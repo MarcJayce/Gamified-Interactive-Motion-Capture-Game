e0aa65c93a10c9823d8a81757cf9054846fadea5
Marc Jayce <Cenidoza.marcjayce0113@gmail.com>
2025-10-22 22:40:43 +0800

    fixed vite config

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // Ensures correct routing on Vercel
   server: {
     proxy: {
       '/api': {
@@ -13,4 +14,4 @@ export default defineConfig({
       }
     }
   }
-})
+});
